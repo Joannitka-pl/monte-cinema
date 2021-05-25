a2be6cbc91636e861b08ad8b102d7b656be33f7b
Joanna Wroblewska <joanna.wroblewska@monterail.com>
2021-05-25 13:31:36 +0200

    update sentry project

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 'https://850aac4d88a84e4faf76b8db323bd16e@o723071.ingest.sentry.io/5781837'
+  config.dsn = 'https://7ccdadd4209d475095eaf9629cb872ba@o723071.ingest.sentry.io/5781864'
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
