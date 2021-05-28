bbd4f6228601f01bd1c846ecb208dfe6835f9f51
Joanna Wroblewska <joanna.wroblewska@monterail.com>
2021-05-28 17:15:56 +0200

    move hall repository to base repository

--- app/concepts/halls/repository.rb
+++ app/concepts/halls/repository.rb
@@ -1,29 +1,8 @@
 module Halls
-  class Repository
-    attr_reader :adapter
+  class Repository < ::BaseRepository
 
     def initialize(adapter: Hall)
-      @adapter = adapter
-    end
-
-    def find_all
-      adapter.order(:id).all
-    end
-
-    def show(id)
-      adapter.find(id)
-    end
-
-    def create(params)
-      adapter.create(params)
-    end
-
-    def update(id, params)
-      adapter.update(id, params)
-    end
-
-    def destroy(id)
-      adapter.destroy(id)
+      super(adapter: adapter)
     end
   end
 end
---
+++ lib/base_repository.rb
@@ -0,0 +1,27 @@
+class BaseRepository
+  attr_reader :adapter
+
+  def initialize(adapter:)
+    @adapter = adapter
+  end
+
+  def find_all
+    adapter.order(:id).all
+  end
+
+  def show(id)
+    adapter.find(id)
+  end
+
+  def create(params)
+    adapter.create(params)
+  end
+
+  def update(id, params)
+    adapter.update(id, params)
+  end
+
+  def destroy(id)
+    adapter.destroy(id)
+  end
+end
