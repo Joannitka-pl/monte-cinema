0dde37c6949a74ecf0be9ac6b8d58665e08e46e9
Joanna Wroblewska <joanna.wroblewska@monterail.com>
2021-05-30 17:18:38 +0200

    correct hall spec

--- spec/requests/halls_spec.rb
+++ spec/requests/halls_spec.rb
@@ -1,10 +1,10 @@
-
 require "rails_helper"
 
 RSpec.describe "Halls requests" do
-  describe "GET /halls" do
-    let!(:hall) { Hall.create(number: "1", capacity: 500) }
 
+  let!(:hall) { Hall.create(number: "1", capacity: 500) }
+
+  describe "GET /halls" do
     it "returns status 200" do
       get("/halls")
       expect(response.status).to eq(200)
@@ -12,8 +12,6 @@ RSpec.describe "Halls requests" do
   end
 
   describe "GET /halls/:id" do
-    let!(:hall) { Hall.create(number: "1", capacity: 500) }
-
     it "returns status 200" do
       get("/halls/#{hall.id}")
       expect(response.status).to eq(200)
@@ -28,8 +26,6 @@ RSpec.describe "Halls requests" do
   end
 
   describe "PUT /halls/:id" do
-    let!(:hall) { Hall.create(number: "1", capacity: 500) }
-
     it "returns status 200" do
       put("/halls/#{hall.id}", params: { hall: { id: hall.id, number: "1" } })
       expect(response.status).to eq(200)
@@ -37,8 +33,6 @@ RSpec.describe "Halls requests" do
   end
 
   describe "DELETE /halls/:id" do
-    let!(:hall) { Hall.create(number: "1", capacity: 500) }
-
     it "returns status 204" do
       delete("/halls/#{hall.id}")
       expect(response.status).to eq(204)
