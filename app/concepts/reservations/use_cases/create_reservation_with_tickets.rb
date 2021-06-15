221286acca9608a276cd32834bac0b2f66809f49
Joanna Wroblewska <joanna.wroblewska@monterail.com>
2021-06-15 19:35:10 +0200

    remove commented line from create reservation with tickets

--- app/concepts/reservations/use_cases/create_reservation_with_tickets.rb
+++ app/concepts/reservations/use_cases/create_reservation_with_tickets.rb
@@ -10,7 +10,6 @@ module Reservations
       def call(params:)
         Reservation.transaction do
           reservation_params = params.except(:sort, :price)
-          # reservation_params = (params[:seat]).split(',')
           ticket_params = params.except(:status, :client_id, :ticket_desk_id, :seance_id, :seat)
           @reservation = @repository.create!(reservation_params)
           Tickets::UseCases::Create.new(reservation: @reservation, ticket_params: ticket_params).call
