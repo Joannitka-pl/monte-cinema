29770697439334f7c89694df59cad9685073c93c
Joanna Wroblewska <joanna.wroblewska@monterail.com>
2021-06-15 14:35:21 +0200

    correct reservation create for mailer

--- app/concepts/reservations/use_cases/create_online.rb
+++ app/concepts/reservations/use_cases/create_online.rb
@@ -9,8 +9,6 @@ module Reservations
 
       def send_confirmation_email
         client = Clients::Repository.new.show(@reservation.client_id)
-        seance = Seances::Repository.new.show(@reservation.seance_id)
-        movie = Movies::Repository.new.show(seance.movie_id)
 
         ClientMailer.with(client).reservation_confirmation_email(reservation: @reservation).deliver_now!
       end
