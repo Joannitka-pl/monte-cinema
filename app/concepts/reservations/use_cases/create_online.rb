ffff3269c9af96fae69195073c458165d3f3ba5a
Joanna Wroblewska <joanna.wroblewska@monterail.com>
2021-06-15 20:21:23 +0200

    move async job to createonline class

--- app/concepts/reservations/use_cases/create_online.rb
+++ app/concepts/reservations/use_cases/create_online.rb
@@ -4,6 +4,18 @@ module Reservations
       def call(params:)
         super
         @reservation
+        cancel_expired_reservation
+      end
+
+      private
+
+      def cancel_expired_reservation
+        CancelReservationJob.set(wait_until: seance_expiry_time).perform_later(@reservation.id) unless @reservation.status == "paid"
+      end
+
+      def seance_expiry_time
+        @seance = Seances::UseCases::Show.new.call(id: @reservation.seance_id)
+        @seance.date - 30.minutes
       end
     end
   end
--- app/concepts/reservations/use_cases/create_reservation_with_tickets.rb
+++ app/concepts/reservations/use_cases/create_reservation_with_tickets.rb
@@ -13,17 +13,7 @@ module Reservations
           ticket_params = params.except(:status, :client_id, :ticket_desk_id, :seance_id, :seat)
           @reservation = @repository.create!(reservation_params)
           Tickets::UseCases::Create.new(reservation: @reservation, ticket_params: ticket_params).call
-
-          CancelReservationJob.set(wait_until: seance_expiry_time).perform_later(@reservation.id) unless @reservation.status == "paid"
-
         end
-      end
-
-      private
-
-      def seance_expiry_time
-        @seance = Seances::UseCases::Show.new.call(id: @reservation.seance_id)
-        @seance.date - 30.minutes
       end
     end
   end
